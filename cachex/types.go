07314ad21edff45a5c782d2087354dc472dbb433
linlinWu <18574945291@163.com>
2025-03-13 16:10:13 +0800

    feat：本地缓存接口设计

--- cachex/types.go
+++ cachex/types.go
@@ -1,4 +1,10 @@
 package cachex
 
-type CacheX interface {
+import "context"
+
+type CacheX[K comparable, V any] interface {
+	Set(ctx context.Context, k K, v V) error
+	Get(ctx context.Context, k K) (V, error)
+	Del(ctx context.Context, k K) error
+	Keys(ctx context.Context) ([]K, error)
 }
